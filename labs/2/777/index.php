c8d39ef95a1f8ea52e5046daf70737f363a1af0a
Jonathan Munz <jmunz@csumb.edu>
2018-09-19 16:40:49 +0000

    Uploading lab 2

--- labs/2/777/index.php
+++ labs/2/777/index.php
@@ -1,13 +1,27 @@
 <!DOCTYPE html>
 <html>
     <head>
+        <style>
+            @import url("css/styles.css"); 
+        </style>
+        
         <title> 777 Slot Machine </title>
     </head>
     <body>
+        <div id="main">
+            
+        
         <?php
         
         include 'inc/functions.php';
+        //play(); 
         
         ?>
+        
+        <form>
+            <input type="submit" value="Spin!" />
+        </form>
+        
+    </div>
     </body>
 </html>
